c45a256edc9e35f0d32ed5ae687dac698fdc7286
VadimVibe <kolesnikov.vadim2007@gmail.com>
2025-10-22 22:33:19 +0300

    print_place()

---
+++ game.py
@@ -0,0 +1,6 @@
+def print_place():
+    print(place[1], '|', place[2], '|', place[3])
+    print('--+---+--')
+    print(place[4], '|', place[5], '|', place[6])
+    print('--+---+--')
+    print(place[7], '|', place[8], '|', place[9])
