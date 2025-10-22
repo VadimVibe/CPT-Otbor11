e88451f9dac9ac08db7f1d329d406c774c280b6d
VadimVibe <kolesnikov.vadim2007@gmail.com>
2025-10-22 22:39:34 +0300

    dannie

--- game.py
+++ game.py
@@ -12,6 +12,7 @@ def print_play_game():
     print(' 4 | 5 | 6 ')
     print('---+---+---')
     print(' 7 | 8 | 9 ')
+    
 
 def if_win():
     for i in range(0,7,3):
@@ -20,3 +21,6 @@ def if_win():
         if place[1+i] == place[4+i] == place[7+i] and place[1+i] != ' ': return 1
     if (place[1] == place[5] == place[9] and place[1] != ' ') or (place[3] == place[5] == place[7] and place[3] != ' '): return 1
     return  0
+
+place,hod_pl,hod, hod_zn = [' ']*10,['первый','второй','третий','четвертый','пятый'], 0, ['О','X']
+name = ['Игрок 1','Игрок 2']
